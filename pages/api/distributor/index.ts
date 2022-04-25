a3e39c8e0086150199464ff0bc3de7084de469fc
Yagel Hayun <yagel1999@gmail.com>
2022-04-25 16:56:40 +0300

    bot endpoint

---
+++ pages/api/distributor/index.ts
@@ -0,0 +1,52 @@
+import twilio, { Twilio } from "twilio";
+import { MessageInstance } from "twilio/lib/rest/api/v2010/account/message";
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const client: Twilio = twilio(process.env.TWILIO_AUTH_TOKEN, process.env.TWILIO_ACCOUNT_SID);
+
+const sendMessage = (text: string, receivingPhoneNumber: string): Promise<MessageInstance> => {
+    return client.messages 
+      .create({ 
+         body: text, 
+         from: 'whatsapp:+14155238886',       
+         to: `whatsapp:${receivingPhoneNumber}` 
+       });
+}
+
+const distribute = async (): Promise<void> => {
+  const people = (await import('./peopleMockData.json')).default;
+  const stages = (await import('./stagesMockData.json')).default;
+  const authorities = (await import('./weaponAuthority.json')).default;
+
+  const approvers = people.filter((person) => person.rank === "admin");
+
+  approvers.forEach(async (approver) => {
+    const stagesToApprove = stages.filter((stage) => 
+        !stage.approvingAuthority &&
+        authorities.some((authority) => 
+            authority.approverId === approver.id && 
+            authority.stageId === stage.id
+        )
+    );
+
+    if (stagesToApprove.length) {
+        const text = `You have ${stagesToApprove.length} active requests.\nFollow the link below to approve 'em all!\nhttps://ynet.co.il`
+        const messageRes = await sendMessage(text, approver.phoneNumber);
+        const { body, from, to, dateCreated, dateSent } = messageRes;
+        
+        console.log({
+          body, from, to, dateCreated, dateSent
+        });
+    } else {
+        console.log(`nothing to approve for ${approver.name}`);
+    }
+  })
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<string>
+) {
+  await distribute();
+  res.status(200).json('hi')
+}
